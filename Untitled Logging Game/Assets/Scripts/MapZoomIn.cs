e7c207655a7dbd31045f0f8d0dff0bd51a6a68c0
Sven <Sven.Sackmann@web.de>
2020-05-07 16:15:33 +0100

    Lean tween and MapPrototype WIP

---
+++ Untitled Logging Game/Assets/Scripts/MapZoomIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapZoomIn : MonoBehaviour
+{
+
+    public Transform zoom1;
+    public Camera mainCam;
+    public float zoomTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Zoom1()
+    {
+        mainCam.transform.LeanMove(zoom1.transform.position, zoomTime);
+        mainCam.transform.LeanRotate(zoom1.rotation.eulerAngles, zoomTime);
+    }
+}
