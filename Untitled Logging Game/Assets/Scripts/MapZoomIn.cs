7ac01cfdfb9d8517590a3d97144a1de8f96b618f
Sven <Sven.Sackmann@web.de>
2020-05-13 11:38:13 +0100

    finished map screen functionality, added textmeshPro
    visuals coming soon

--- Untitled Logging Game/Assets/Scripts/MapZoomIn.cs
+++ Untitled Logging Game/Assets/Scripts/MapZoomIn.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapZoomIn : MonoBehaviour
 {
 
-    public Transform zoom1;
+    public Transform[] zooms = new Transform[3];
     public Camera mainCam;
     public float zoomTime;
+    public string[] sceneLoads = new string[3];
+    public Transform defaultZoom;
+    private SceneMan sceneMan;
+
+    private void Awake()
+    {
+        sceneMan = FindObjectOfType<SceneMan>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +33,82 @@ public class MapZoomIn : MonoBehaviour
 
     public void Zoom1()
     {
-        mainCam.transform.LeanMove(zoom1.transform.position, zoomTime);
-        mainCam.transform.LeanRotate(zoom1.rotation.eulerAngles, zoomTime);
+        mainCam.transform.LeanMove(zooms[0].transform.position, zoomTime);
+        mainCam.transform.LeanRotate(zooms[0].rotation.eulerAngles, zoomTime);
+        StartCoroutine(LoadScene(sceneLoads[0]));
+    }
+    
+    public void Zoom2()
+    {
+        mainCam.transform.LeanMove(zooms[1].transform.position, zoomTime);
+        mainCam.transform.LeanRotate(zooms[1].rotation.eulerAngles, zoomTime);
+        StartCoroutine(LoadScene(sceneLoads[1]));
+    }
+    
+    public void Zoom3()
+    {
+        mainCam.transform.LeanMove(zooms[2].transform.position, zoomTime);
+        mainCam.transform.LeanRotate(zooms[2].rotation.eulerAngles, zoomTime);
+        StartCoroutine(LoadScene(sceneLoads[2]));
+    }
+
+    public void ZoomOut()
+    {
+        Debug.Log("Old position was " + mainCam.transform.position.x + "  " + mainCam.transform.position.y + "  " + mainCam.transform.position.z);
+        mainCam.transform.LeanMove(defaultZoom.transform.position, zoomTime);
+        mainCam.transform.LeanRotate(defaultZoom.rotation.eulerAngles, zoomTime);
+    }
+    
+    IEnumerator LoadScene(string sceneName)
+    {
+        float timeElapsed = 0;
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        asyncLoad.allowSceneActivation = false;
+        sceneMan.prevScene = SceneManager.GetActiveScene().name;
+        while(!asyncLoad.isDone && timeElapsed < zoomTime)
+        {
+            // Debug.Log(" Time remaining is " + (zoomTime-timeElapsed));
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        asyncLoad.allowSceneActivation = true;
+    }
+
+    IEnumerator ReturnToMap(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (sceneMan.prevScene == sceneLoads[0])
+        {
+            mainCam.transform.position = zooms[0].position;
+            mainCam.transform.rotation = zooms[0].rotation;
+        }
+        else if (sceneMan.prevScene == sceneLoads[1])
+        {
+            mainCam.transform.position = zooms[1].position;
+            mainCam.transform.rotation = zooms[1].rotation;
+        }
+        else if (sceneMan.prevScene == sceneLoads[2])
+        {
+            mainCam.transform.position = zooms[2].position;
+            mainCam.transform.rotation = zooms[2].rotation;
+        }
+        ZoomOut();
+        // Debug.Log("Zoom me out, scotty !");
+    }
+    
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+         
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+         
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        // Debug.Log("I totally happen");
+        StartCoroutine(ReturnToMap(.01f));
     }
 }
---
+++ Untitled Logging Game/Assets/Scripts/SceneMan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMan : MonoBehaviour
+{
+    
+    public static SceneMan instance = null;
+
+    public string prevScene = "";
+
+    void Awake() {
+        if(!instance )
+            instance = this;
+        else {
+            Destroy(this.gameObject) ;
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject) ;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
