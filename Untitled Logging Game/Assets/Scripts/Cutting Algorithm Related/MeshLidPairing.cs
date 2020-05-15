88d6c2d5e974e78abae74ecb40866f1f99645634
RaphaelPriatama <blue.person16@gmail.com>
2020-05-15 19:52:22 +0200

    Physics Implemented with mesh colliders

---
+++ Untitled Logging Game/Assets/Scripts/Cutting Algorithm Related/MeshLidPairing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class MeshLidPairing
+{
+
+    public IndividualVertex v0;
+    public IndividualVertex v1;
+
+    public MeshLidPairing(IndividualVertex v0, IndividualVertex v1)
+    {
+        this.v0 = v0;
+        this.v1 = v1;
+    }
+
+    public IndividualTriangle CreateTriangle(IndividualVertex centerVertex)
+    {
+        return new IndividualTriangle(v0, centerVertex, v1);
+    }
+
+
+}
