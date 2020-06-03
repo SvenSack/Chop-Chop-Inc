d21ec6e99080ef0531dabe7e7dad88f9ec7069f3
RaphaelPriatama <blue.person16@gmail.com>
2020-06-03 15:08:18 +0200

    Added a Scoreboard Manager

---
+++ Untitled Logging Game/Assets/Scripts/GameIO/GameDataWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameDataWriter
+{
+    private BinaryWriter writer;
+
+    public GameDataWriter(BinaryWriter writer)
+    {
+        this.writer = writer;
+    }
+
+    public void Write(float value)
+    {
+        writer.Write(value);
+    }
+
+    public void Write(int value)
+    {
+        writer.Write(value);
+    }
+
+    public void Write(string value)
+    {
+        writer.Write(value);
+    }
+
+
+
+}
---
+++ Untitled Logging Game/Assets/Scripts/GameIO/PlayerScoreData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScoreData
+{
+    public string Name;
+    public float Score;
+
+
+
+    public PlayerScoreData(string name, float score)
+    {
+        this.Name = name;
+        this.Score = score;
+    }
+
+    public PlayerScoreData() { }
+
+    public void Save(GameDataWriter dataWriter)
+    {
+        dataWriter.Write(Name);
+        dataWriter.Write(Score);
+
+        Debug.Log("Saved " + Name + "," + Score);
+
+    }
+
+    public void Load(GameDataReader dataReader)
+    {
+        Name = dataReader.ReadString();
+        Score = dataReader.ReadFloat();
+
+        Debug.Log("Loaded " + Name + "," + Score);
+
+
+    }
+
+
+}
+
+public class PlayerScoreComparer : IComparer<PlayerScoreData>
+{
+    public int Compare(PlayerScoreData a, PlayerScoreData b)
+    {
+        return b.Score.CompareTo(a.Score);
+    }
+}
