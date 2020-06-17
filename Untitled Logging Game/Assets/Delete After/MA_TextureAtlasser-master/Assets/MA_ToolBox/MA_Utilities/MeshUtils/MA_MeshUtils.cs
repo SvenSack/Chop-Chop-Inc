7df450bed06ab543e64a6d1e192a5f47047aa4f4
Petar <petart.zdravkov@gmail.com>
2020-06-17 16:23:54 +0300

    atlas for amazon and finland

---
+++ Untitled Logging Game/Assets/Delete After/MA_TextureAtlasser-master/Assets/MA_ToolBox/MA_Utilities/MeshUtils/MA_MeshUtils.cs
@@ -0,0 +1,174 @@
+﻿//https://github.com/maxartz15/MA_MeshUtils
+
+//References:
+//http://wiki.unity3d.com/index.php?title=ObjExporter
+
+#if UNITY_EDITOR
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace MA_Mesh
+{
+	public static class MA_MeshUtils
+	{
+		public static string MA_SaveMeshAsset(Mesh mesh, string savePath)
+		{
+			if (string.IsNullOrEmpty(mesh.name))
+			{
+				mesh.name = UnityEngine.Random.Range(11111, 99999).ToString();
+			}
+
+			string assetPath = savePath + mesh.name + ".asset";
+
+			AssetDatabase.CreateAsset(mesh, assetPath);
+			AssetDatabase.SaveAssets();
+
+			return assetPath;
+		}
+
+		public static string MA_SaveMeshPrefab(Mesh mesh, string prefabName, string savePath, string materialPath)
+		{
+			string assetPath = null;
+
+			string meshAssetPath = MA_SaveMeshAsset(mesh, savePath);
+			Mesh meshAsset = AssetDatabase.LoadAssetAtPath<Mesh>(meshAssetPath);
+
+			if (meshAsset != null)
+			{
+				GameObject gameObject = new GameObject
+				{
+					name = prefabName
+				};
+
+				gameObject.AddComponent<MeshFilter>().mesh = meshAsset;
+				gameObject.AddComponent<MeshRenderer>();
+
+				Material curMaterial = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+				if (curMaterial != null)
+				{
+					gameObject.GetComponent<MeshRenderer>().material = curMaterial;
+				}
+
+				if (string.IsNullOrEmpty(prefabName))
+				{
+					prefabName = UnityEngine.Random.Range(11111, 99999).ToString();
+				}
+
+				assetPath = savePath + prefabName + ".prefab";
+
+				PrefabUtility.SaveAsPrefabAsset(gameObject, assetPath);
+				UnityEngine.Object.DestroyImmediate(gameObject);
+			}
+
+			return assetPath;
+		}
+
+		public static Mesh MA_DuplicateMesh(Mesh mesh)
+		{
+			Mesh newMesh = new Mesh
+			{
+				name = mesh.name,
+				bounds = mesh.bounds,
+				subMeshCount = mesh.subMeshCount
+			};
+
+			newMesh.SetVertices(new List<Vector3>(mesh.vertices));
+			for (int i = 0; i < mesh.subMeshCount; i++)
+			{
+				newMesh.SetTriangles(mesh.GetTriangles(i), i);
+			}
+			newMesh.SetNormals(new List<Vector3>(mesh.normals));
+			newMesh.SetUVs(0, new List<Vector2>(mesh.uv));
+			newMesh.SetTangents(new List<Vector4>(mesh.tangents));
+			newMesh.SetColors(new List<Color>(mesh.colors));
+
+			return newMesh;
+		}
+
+		public static Mesh MA_ReMapUV(this Mesh mesh, Vector2 atlasSize, Vector2 textureSize, Vector2 texturePosition, int uvChannel = 0)
+		{
+			/*
+			 0     1
+			512 x 512
+
+			 0    .5 	= 1 / 512 * 256
+			256 x 256
+
+			+ pos
+			*/
+
+			List<Vector2> uvs = new List<Vector2>();
+
+			//Get UV's
+			mesh.GetUVs(uvChannel, uvs);
+
+			foreach (Vector2 uvCordinate in uvs)
+			{
+				float x = (uvCordinate.x / atlasSize.x * textureSize.x) + texturePosition.x;
+				float y = (uvCordinate.y / atlasSize.y * textureSize.y) + texturePosition.y;
+				uvCordinate.Set(x, y);
+			}
+
+			mesh.SetUVs(uvChannel, uvs);
+
+			return mesh;
+		}
+
+		public static Mesh MA_UVReMap(this Mesh mesh, Vector2 atlasSize, Rect textureRect, int uvChannel = 0, bool flipY = true, bool wrap = true)
+		{
+			//Get UV's
+			List<Vector2> uvs = new List<Vector2>();
+			mesh.GetUVs(uvChannel, uvs);
+
+			//Min and max bounds in 0-1 space.
+			float xMin, xMax, yMin, yMax;
+			xMin = (1f / atlasSize.x * textureRect.width);
+			xMax = (1f / atlasSize.x * textureRect.x);
+			yMin = (1f / atlasSize.y * textureRect.height);
+
+			//Flip uv's if needed.
+			if (flipY)
+			{
+				yMax = (1f / atlasSize.y * (atlasSize.y - textureRect.height - textureRect.y));
+			}
+			else
+			{
+				yMax = (1f / atlasSize.y * textureRect.y);
+			}
+
+			for (int i = 0; i < uvs.Count; i++)
+			{
+				float newX = uvs[i].x * xMin + xMax;
+				float newY = uvs[i].y * yMin + yMax;
+
+				//Wrap the verts outside of the uv space around back into the uv space.
+				if (wrap)
+				{
+					newX = Wrap(newX, xMax, xMin + xMax);
+					newY = Wrap(newY, yMax, yMin + yMax);
+				}
+
+				uvs[i] = new Vector2(newX, newY);
+			}
+
+			mesh.SetUVs(uvChannel, uvs);
+
+			return mesh;
+		}
+
+		public static float Wrap(float val, float min, float max)
+		{
+			val -= (float)Math.Round((val - min) / (max - min)) * (max - min);
+			if (val < min)
+				val = val + max - min;
+			return val;
+		}
+	}
+}
+#endif
---
+++ Untitled Logging Game/Assets/Delete After/MA_TextureAtlasser-master/README.md
@@ -0,0 +1,27 @@
+[![Image](https://maxartz15.com/wp-content/uploads/2020/04/MA_TextureAtlas2.png)](https://maxartz15.com/wp-content/uploads/2020/04/MA_TextureAtlas2.png)
+
+# MA_TextureAtlasser
+Texture atlas creator tool for Unity. <br> This tool is made to combine textures and/or remap the UV’s for 3D models. The tool can also be used to make 2D sprite sheets. The visual editor gives you the ability to set and prioritize the sizes and positions in the texture atlas/sprite sheet.
+- Combine textures/sprites.
+- Automatically adjusts the UV's of the assigned meshes to match the new texture atlas.
+
+### Download unitypackage
+[![Github All Releases](https://img.shields.io/github/downloads/maxartz15/MA_TextureAtlasser/total.svg)](https://github.com/maxartz15/MA_TextureAtlasser/releases) 
+
+### Unity versions
+- Latest release requires Unity 2018.3 or higher.
+- Releases before 1.8 should work with older Unity versions.
+- Upgrading to 1.8+ will break existing atlasses.
+
+## Export options
+### Meshes
+- UnityMesh (with prefab setup)
+### Textures
+- PNG
+- PNG (sliced) sprite sheet
+### Materials
+- Material (with assigned textures and selected shader)
+
+## Resources
+[Youtube video](https://youtu.be/PBRKlopkZP0) <br>
+[Website](https://maxartz15.com/ma_textureatlas/)
