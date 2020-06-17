6eeafdcd19521a6f8fd4535439cde148f238f94e
Sven <Sven.Sackmann@web.de>
2020-06-17 11:41:17 +0100

    finland easy part done

---
+++ Untitled Logging Game/Assets/Scripts/BillboardOrienter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardOrienter : MonoBehaviour
+{
+    private Vector3 cameraPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPosition = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cameraPosition);
+    }
+}
