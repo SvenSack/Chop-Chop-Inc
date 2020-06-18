2b769c3fce0d16eb8741c16d22cbc5901abc34aa
RaphaelPriatama <blue.person16@gmail.com>
2020-06-18 14:06:27 +0200

    Burn Down Mechanic works

---
+++ Untitled Logging Game/Assets/Scripts/GameObjectActivator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectActivator : MonoBehaviour
+{
+    public GameObject objectToActivate;
+
+    public void ActivateObject()
+    {
+        objectToActivate.SetActive(true);
+    }
+  
+}
---
+++ Untitled Logging Game/Assets/Scripts/TerrainLayerSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TerrainLayerSwitcher : MonoBehaviour
+{
+    public Terrain terrain;
+
+    public void Start()
+    {
+        if(!terrain)
+        {
+            terrain = Terrain.activeTerrain;
+        }
+         
+    }
+
+    public void SwitchTerrainAtIndexWith(int switchIndex,TerrainLayer terrainLayer)
+    {
+        TerrainLayer[] layers = terrain.terrainData.terrainLayers;
+
+        if(switchIndex > layers.Length) { return; }
+
+        layers[switchIndex] = terrainLayer;
+
+        terrain.terrainData.terrainLayers = layers;
+
+    }
+
+    public void SwitchTerrain(TerrainLayer terrainLayer, TerrainLayer newterrainLayer)
+    {
+        Debug.LogError("Switch Terain Not Implemented");
+    }
+
+}
