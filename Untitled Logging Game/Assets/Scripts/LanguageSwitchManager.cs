e27250fc7dc592d18ddd3e4eefd3dd942f8117e6
RaphaelPriatama <blue.person16@gmail.com>
2020-06-22 09:11:34 +0200

    language

---
+++ Untitled Logging Game/Assets/Scripts/LanguageSwitchManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Language
+{
+    English,
+    Dutch,
+    German
+}
+
+public class LanguageSwitchManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("language"))
+        {
+            PlayerPrefs.SetInt("language",(int)Language.Dutch);
+        }
+    }
+
+    public void SwitchLanguageTo(Language language)
+    {
+        PlayerPrefs.SetInt("language", (int)language);
+    }
+
+    public void SwitchLanguageToDutch()
+    {
+        SwitchLanguageTo(Language.Dutch);
+    }
+
+    public void SwitchLanguageToGerman()
+    {
+        SwitchLanguageTo(Language.German);
+    }
+
+    public void SwitchLanguageToEnglish()
+    {
+        SwitchLanguageTo(Language.English);
+    }
+}
