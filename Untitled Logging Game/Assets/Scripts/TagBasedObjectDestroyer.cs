8b93bde5d2b9c281d530a807da4f2b6e45779733
RaphaelPriatama <blue.person16@gmail.com>
2020-06-24 14:24:13 +0200

    objects wit foliage tag are destroyed

---
+++ Untitled Logging Game/Assets/Scripts/TagBasedObjectDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagBasedObjectDestroyer : MonoBehaviour
+{
+    public string tag = "foliage";
+
+    private GameObject[] gameObjectsWithTag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectsWithTag = GameObject.FindGameObjectsWithTag(tag);
+    }
+
+    public void DestroyObjectsWithSetTag()
+    {
+        foreach(GameObject taggedObject in gameObjectsWithTag)
+        {
+            Destroy(taggedObject);
+        }
+    }
+}
